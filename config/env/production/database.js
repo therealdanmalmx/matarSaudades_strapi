8478e4357063cd52c3b7ac80f413023c15865c8f
therealdanmalmx <danmalmx@gmail.com>
2023-02-11 21:06:19 +0100

    Test traditional config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,18 @@
 // path: ./config/database.js
-const { parse } = require("pg-connection-string");
-module.exports = ({ env }) => {
-  const { host, port, user, password } = parse(env, "DATABASE_URL");
-  return {
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client: "postgres",
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-      },
-      debug: false,
+      host: env(
+        "DATABASE_HOST",
+        "primary.database--9dq4vh6zrxzp.addon.code.run"
+      ),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "_c05c7eca8974"),
+      user: env("DATABASE_USERNAME", "_a7a01b096b61db14"),
+      password: env("DATABASE_PASSWORD", "_43662da0dc34f5ac94ab747d01517b"),
     },
-  };
-};
+    debug: false,
+  },
+});

bfb7d610c20b250295770d61fe554a4f4deb833d
therealdanmalmx <danmalmx@gmail.com>
2023-02-11 21:23:06 +0100

    Remove hardcoded database connections

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -4,14 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "primary.database--9dq4vh6zrxzp.addon.code.run"
-      ),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "_c05c7eca8974"),
-      user: env("DATABASE_USERNAME", "_a7a01b096b61db14"),
-      password: env("DATABASE_PASSWORD", "_43662da0dc34f5ac94ab747d01517b"),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
     },
     debug: false,
   },
